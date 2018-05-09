292045d3361523267736e3dd259e29135f67de21
liuhui <liuhui244671426@126.com>
2018-05-09 15:14:35 +0800

    添加 sso 配置文件

---
+++ config/sso.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'server' => array(
+        // 登陆跳转页
+        'url_login' => 'http://sso.com/?m=sso&c=center&a=index',
+        // token 检测
+        'url_check' => 'http://sso.com/?m=sso&c=center&a=check',
+    ),
+    //'client' => array(
+        //'callback' => ''
+    //),
+);
