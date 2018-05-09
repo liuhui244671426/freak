6a9e13a7d1a461b7a38a0f284df62aa55ea02973
liuhui <liuhui244671426@126.com>
2018-05-09 15:44:51 +0800

    添加 sso 配置文件

--- config/sso.php
+++ config/sso.php
@@ -2,9 +2,9 @@
 return array(
     'server' => array(
         // 登陆跳转页
-        'url_login' => 'http://sso.com/?m=sso&c=center&a=index',
+        'url_login' => 'http://sso.com/?m=sso&c=server&a=index',
         // token 检测
-        'url_check' => 'http://sso.com/?m=sso&c=center&a=check',
+        'url_check' => 'http://sso.com/?m=sso&c=server&a=check',
     ),
     'cookie_expire' => time()+86400,
     //'client' => array(
