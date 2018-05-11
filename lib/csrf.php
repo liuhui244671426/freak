9f429047dbe97116c34e1fbf825ff6ff242d849b
liuhui <liuhui244671426@126.com>
2018-05-11 18:53:02 +0800

    添加 csrf

---
+++ lib/csrf.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @file
+ *
+ * CSRF
+ */
+
+class csrfLib
+{
+    public static function getHiddenInputString()	{
+        $uuid = microtime(true)*10000;
+        return sprintf('<input type="hidden" name="_csrf_token_uuid" value="%s"/><input type="hidden" name="_csrf_token" value="%s"/>', $uuid, self::generate_token($uuid));
+    }
+
+    public static function generate_token($uuid)
+    {
+        $token = sha1( $uuid.'20180511'.helperLib::getip());
+        return $token;
+    }
+
+    public static function check_token($uuid, $token){
+        $n_token = sha1( $uuid.'20180511'.helperLib::getip());
+        if($n_token == $token){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
--- lib/helperLib.php
+++ lib/helperLib.php
@@ -26,4 +26,14 @@ class helperLib{
         }
         return false;
     }
+
+    public static function randomString( $length )
+    {
+        $seed = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijqlmnopqrtsuvwxyz0123456789';
+        $max = strlen( $seed ) - 1;
+        $string = '';
+        for ( $i = 0; $i < $length; ++$i )
+            $string .= $seed{intval( mt_rand( 0.0, $max ) )};
+        return $string;
+    }
 }
