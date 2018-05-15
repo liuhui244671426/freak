06f3a4d096d2e63c336dd300c7a67a7b0a4f142b
liuhui <liuhui244671426@126.com>
2018-05-15 14:24:23 +0800

    注销 request 库

--- index.php
+++ index.php
@@ -4,7 +4,9 @@
  * @author: liuhui
  * @version: 1.0.0
  */
+//--------Access-----------
 define('FREAK_ACCESS', true);
+//--------Access-----------
 header("Content-type:text/html;charset=utf-8");
 if(version_compare(PHP_VERSION, '5.6.0') < 0){ exit('PHP版本需要大于5.6.0'); }
 
--- lib/request.php
+++ lib/request.php
@@ -4,38 +4,38 @@
  *
  */
 defined('FREAK_ACCESS') or exit('Access Denied');
-class lib_request{
-
-    public static function get($type, $key,$default=''){
-        $params = self::getAll();
-        $params = $params[$key]?$params[$key]:$default;
-        return self::checkParam($type, $params);
-    }
-
-    public static function getAll(){
-        return $_POST?$_POST:$_GET;
-    }
-    //参数检测
-    public static function checkParam($type, $params){
-        switch($type){
-            case 'string':{
-                return strval($params);break;
-            }
-            case 'int': {
-                return intval($params);break;
-            }
-            case 'float':{
-                return floatval($params);break;
-            }
-            case 'array':{
-                array_walk($params, function(&$v, $k){
-                    strval($v);
-                });
-                return $params;
-                break;
-            }
-            default:
-                break;
-        }
-    }
-}
+//class lib_request{
+//
+//    public static function get($type, $key,$default=''){
+//        $params = self::getAll();
+//        $params = $params[$key]?$params[$key]:$default;
+//        return self::checkParam($type, $params);
+//    }
+//
+//    public static function getAll(){
+//        return $_POST?$_POST:$_GET;
+//    }
+//    //参数检测
+//    public static function checkParam($type, $params){
+//        switch($type){
+//            case 'string':{
+//                return strval($params);break;
+//            }
+//            case 'int': {
+//                return intval($params);break;
+//            }
+//            case 'float':{
+//                return floatval($params);break;
+//            }
+//            case 'array':{
+//                array_walk($params, function(&$v, $k){
+//                    strval($v);
+//                });
+//                return $params;
+//                break;
+//            }
+//            default:
+//                break;
+//        }
+//    }
+//}
