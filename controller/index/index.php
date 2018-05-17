82c7dce1af5f52abdfb7a16ee693e6da29aacb4a
liuhui <liuhui244671426@126.com>
2018-05-17 11:10:08 +0800

    调整

--- controller/index/index.php
+++ controller/index/index.php
@@ -49,6 +49,28 @@ class controller_index_index extends controller_base{
 //        $r = new core_redis();
 //        $ret = $r->get('PHPREDIS_SESSION:'.session_id());
         //phpinfo();
-        echo lib_filter::session('aaa');
+        //echo lib_filter::session('aaa');
+        $this->welcome();
     }
+
+    public function welcome(){
+        core_output::view_layout_render('fontend/welcome', array(), 'fontend/layout');
+    }
+
+    public function adopt(){
+        $model = new model_adopt();
+        $list = $model->getList();
+        core_output::view_layout_render('fontend/adopt', array('list' => $list), 'fontend/layout');
+    }
+
+    public function adopt_detail(){
+        $adopt_id = lib_filter::intGet('adopt_id');
+        $model = new model_adopt();
+        $info = $model->getById($adopt_id);
+        core_output::view_layout_render('fontend/adopt_detail', array('info' => $info), 'fontend/layout');
+    }
+    public function show(){
+
+    }
+
 }
--- model/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class model_index extends model_base{
-    public function get_name(){
-        return __METHOD__;
-    }
-}
