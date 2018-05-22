f69342efcfe0895acc61e12816e7389c0cacc099
liuhui <liuhui244671426@126.com>
2018-05-22 17:20:35 +0800

    fix debug.php

--- debug.php
+++ debug.php
@@ -7,20 +7,21 @@ $is_debug = true;
 define('FREAK_ACCESS', true);
 header("Content-type:text/html;charset=utf-8");
 if(version_compare(PHP_VERSION, '5.6.0') < 0){ exit('PHP版本需要大于5.6.0'); }
-if($is_debug) { xhprof_enable(XHPROF_FLAGS_CPU+XHPROF_FLAGS_MEMORY); }
-
+//-----------------------------------------
+xhprof_enable(XHPROF_FLAGS_CPU+XHPROF_FLAGS_MEMORY);
+//-----------------------------------------
 include dirname(__FILE__).'/bootstrap.php';
 
 run();
-if($is_debug){
-    $xhprof_data = xhprof_disable();
-    $xhprof_root = "/Users/liuhui/vagrant/htdocs/xhprof";
-    include_once $xhprof_root."/xhprof_lib/utils/xhprof_lib.php";
-    include_once $xhprof_root."/xhprof_lib/utils/xhprof_runs.php";
-    $xhprof_runs = new XHProfRuns_Default();
-    $run_id = $xhprof_runs->save_run($xhprof_data, "xhprof_freak");
-    $u = 'http://xhprof.com/?run=' . $run_id . '&source=xhprof_freak';
-    echo "<a href='{$u}' target='_blank'>xhprof</a>";
-}
+//-----------------------------------------
+$xhprof_data = xhprof_disable();
+$xhprof_root = "/Users/liuhui/vagrant/htdocs/xhprof";
+include_once $xhprof_root."/xhprof_lib/utils/xhprof_lib.php";
+include_once $xhprof_root."/xhprof_lib/utils/xhprof_runs.php";
+$xhprof_runs = new XHProfRuns_Default();
+$run_id = $xhprof_runs->save_run($xhprof_data, "xhprof_freak");
+$u = 'http://xhprof.com/?run=' . $run_id . '&source=xhprof_freak';
+echo "<a href='{$u}' target='_blank'>xhprof</a>";
+//-----------------------------------------
 
 return;
