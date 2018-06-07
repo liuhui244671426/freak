8079917ea135844ae43db040e047683971152454
liuhui <liuhui244671426@126.com>
2018-06-07 18:54:28 +0800

    后台 layout

--- controller/admin/index.php
+++ controller/admin/index.php
@@ -6,7 +6,7 @@ class controller_admin_index extends controller_base{
     }
 
     public function index(){
-
+        freak_output::view_layout_render('admin/index/index', array(), 'admin/layout');
     }
 
     public function welcome(){
