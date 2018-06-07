0fc674a8deaf8680ecbae6f4bf1b76131096c517
liuhui <liuhui244671426@126.com>
2018-06-07 16:21:47 +0800

    添加远程静态地址库

---
+++ config/common.php
@@ -0,0 +1,5 @@
+<?php
+return array(
+    //静态文件地址前缀
+    'static' => 'http://10.222.66.180:8888/yuntheme/www.yuntheme.com/yun/',
+);
--- controller/admin/index.php
+++ controller/admin/index.php
@@ -1,15 +1,15 @@
 <?php
 
 class controller_admin_index extends controller_base{
-    public function init(){}
+    public function init(){
+        $bool = lib_ssoClient::is_login();
+        if(!$bool) {
+            exit('404');
+        }
+    }
 
     public function index(){
-        $bool = lib_ssoClient::is_login();
-        if($bool){
-            echo 'success';
-        } else {
-            echo 'failed';
-        }
+
     }
 
     public function welcome(){
