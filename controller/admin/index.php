fa4d72805afa2c04e991bf95f80c77f5fe73cfe9
liuhui <liuhui244671426@126.com>
2018-06-12 11:55:31 +0800

    fix 基本功能

--- controller/admin/index.php
+++ controller/admin/index.php
@@ -7,9 +7,23 @@ class controller_admin_index extends controller_base{
 
     public function index(){
         freak_output::view_layout_render('admin/index/index', array(), 'admin/layout');
+        return;
     }
-    public function welcome(){}
-
+    public function show_one(){
+        $id = lib_filter::get('id');
+        $db = new freak_pdo('read');
+        $ret = $db->query("select * from `content` where id=:id", array('id' => $id));
+        freak_output::view_layout_render('admin/index/show', array('text' => $ret[0]), 'admin/layout');
+        return;
+    }
+    public function show_list(){
+        $page = lib_filter::get('page', 0);
+        $start = $page * 10;
+        $db = new freak_pdo('read');
+        $ret = $db->query("select * from `content` order by id desc limit :start, 10", array('start' => $start));
+        freak_output::view_layout_render('admin/index/show_list', array('text' => $ret), 'admin/layout');
+        return;
+    }
     public function editor(){
         $pre = freak_config::get('common', 'static');
         $css = [
@@ -29,5 +43,7 @@ class controller_admin_index extends controller_base{
     }
     public function editor_post(){
         print_r($_POST);
+        $db = new freak_pdo('write');
+        $db->insert('content', array('content' => $_POST['editordata']));
     }
 }
---
+++ model/editor.php
@@ -0,0 +1,6 @@
+<?php
+class model_editor extends model_base{
+    public function create($insert_array){
+
+    }
+}
