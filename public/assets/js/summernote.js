755c39571e97dd30a2dc2ca61ddfca122faf17ab
liuhui <liuhui244671426@126.com>
2018-06-12 11:03:33 +0800

    修复 summernote

--- public/assets/js/summernote.js
+++ public/assets/js/summernote.js
@@ -14,13 +14,21 @@ $(document).ready(function(){
                 cache: false,
                 processData: false,
                 success: function (data) {
-                    if(data['status'] == 1) {
-                        //把图片放到编辑框中。editor.insertImage 是参数，写死。后面是图片资源路径。
-                        $('.summernote').summernote('editor.insertImage', data['data'][0]['savepath']+data['data'][0]['savename']);
+
+                    if(isJSON(data) == true){
+                        //上传图片资源
+                        if(data['status'] == 1) {
+                            //把图片放到编辑框中。editor.insertImage 是参数，写死。后面是图片资源路径。
+                            $('.summernote').summernote('editor.insertImage', data['data'][0]['savepath']+data['data'][0]['savename']);
+                        }
+                        if(data['status'] == -1) {
+                            console.error(data['data']);
+                        }
+                    } else {
+                        //图片地址
+                        $('.summernote').summernote('editor.insertImage', data);
                     }
-                    if(data['status'] == -1) {
-                        console.error(data['data']);
-                    }
+
                 },
                 error: function () {
                     alert("上传失败");
@@ -29,4 +37,14 @@ $(document).ready(function(){
             });
         }
     });
+    //是否是 json 字符串
+    function isJSON(str) {
+        if (typeof str == 'string') {
+            if (typeof JSON.parse(str) == 'object'){
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
 });
