e7a78692fed0f8fc9c9cc9297e7e2035ab9e36ea
刘辉 <liuhui9@staff.weibo.com>
2018-11-23 15:23:15 +0800

    fix

--- config/redis.php
+++ config/redis.php
@@ -1,34 +1,11 @@
 <?php
 defined('FREAK_ACCESS') or exit('Access Denied');
-return array(
-    'product' => array(
-        'read' => array(),
-        'write' => array(),
-    ),
-    'develop' => array(
-        'freak' => array(
-            'read' => array(
-                'host' => '',
-                'port' => '',
-                'password' => '',
-            ),
-            'write' => array(
-                'host' => '',
-                'port' => '',
-                'password' => '',
-            ),
-        ),
-        'weibo' => array(
-            'read' => array(
-                'host' => '',
-                'port' => '',
-                'password' => '',
-            ),
-            'write' => array(
-                'host' => '',
-                'port' => '',
-                'password' => '',
-            ),
-        ),
-    )
-);
+return [
+    'product' => [
+        'read' => [],
+        'write' => [],
+    ],
+    'develop' => [
+        
+    ]
+];
