752e72eb88a14e723dc15da0a4c233425b366ee2
刘辉 <liuhui9@staff.weibo.com>
2018-12-12 15:29:02 +0800

    优化构建工具

--- build.php
+++ build.php
@@ -7,66 +7,34 @@ define('DS', DIRECTORY_SEPARATOR);
 
 echo 'Freak 框架构建工具'.PHP_EOL.PHP_EOL;
 
-$mode_map = [
-    1 => 'fpm',
-    2 => 'daemon',
-    11 => 'delete logs',
-];
+$mode_map = [1 => 'fpm', 2 => 'daemon', 11 => 'clear',];
 fwrite(STDOUT, '选择模式
 1:创建网页接口
 2:创建任务脚本
-11:清理日志
+11:清理缓存
 请输入数字: ');
-$mode = trim(fgets(STDIN));
-$mode = $mode_map[$mode];
+$input = trim(fgets(STDIN));
+$mode = $mode_map[$input];
 if($mode == NULL) {
     echo "ERROR mode NOT EXISTS";return;
 }
-if($mode == 'delete logs'){
-    $op = opendir(PATH_ROOT.DS.'logs');
-    while(false !== ($file = readdir($op))){
-        if($file == '.' || $file == '..'){
-            continue;
+
+if($input == 11){
+    foreach ([PATH_ROOT.DS.'logs', PATH_ROOT.DS.'download'.DS.'images'] as $path) {
+        $res = clear_cache($path);
+        if($res){
+            echo "SUCCESS clean dir: {$path} ".PHP_EOL;
         }
-        unlink(PATH_ROOT.DS.'logs'.DS.$file);
-    }
-    closedir($op);
-    echo "SUCCESS clean logs".PHP_EOL;return;
-}
-
-fwrite(STDOUT, '请输入 module: ');
-$module = trim(fgets(STDIN));
-fwrite(STDOUT, '请输入 controller: ');
-$controller = trim(fgets(STDIN));
-
-
-$module_dir_path = PATH_ROOT.DS.$mode.DS.$module;
-$controller_file_path = $module_dir_path.DS.$controller.'.php';
-
-if(!is_dir($module_dir_path)){
-    if(!mkdir($module_dir_path, 0777, true)){
-        echo "ERROR mkdir {$module_dir_path} FAILED";return;
     }
 }
-if(file_exists($controller_file_path)){
-    echo "ERROR file {$controller_file_path} is EXISTS, please DELETE";return;
-}
+if($input == 1 || $input == 2){
+    fwrite(STDOUT, '请输入 module: ');
+    $module = trim(fgets(STDIN));
+    fwrite(STDOUT, '请输入 controller: ');
+    $controller = trim(fgets(STDIN));
 
-$fp = fopen($controller_file_path, 'a+');
-$fw = false;
-if($mode == 'fpm'){
-    $fw = fwrite($fp, build_web_class($module, $controller));
+    make_file($mode, $module, $controller);
 }
-if($mode == 'daemon'){
-    $fw = fwrite($fp, build_cli_class($module, $controller));
-}
-fclose($fp);
-
-if($fw === false){
-    echo "ERROR write file {$controller_file_path} is FAILED";return;
-}
-
-echo "SUCCESS write file {$controller_file_path}".PHP_EOL;
 return;
 
 
@@ -103,4 +71,50 @@ class daemon_{$m}_{$c} extends daemon_workers_base {
 new daemon_{$m}_{$c}();
     ";
     return $tmp;
+}
+
+function clear_cache($path){
+    $op = opendir($path);
+    while(false !== ($file = readdir($op))){
+        if($file == '.' || $file == '..'){
+            continue;
+        }
+        unlink($path.DS.$file);
+    }
+    closedir($op);
+    return true;
+}
+
+function make_file($mode, $module, $controller){
+    $module_dir_path = PATH_ROOT.DS.$mode.DS.$module;
+    $controller_file_path = $module_dir_path.DS.$controller.'.php';
+
+    if(!is_dir($module_dir_path)){
+        if(!mkdir($module_dir_path, 0777, true)){
+            echo "ERROR mkdir {$module_dir_path} FAILED";
+            return false;
+        }
+    }
+    if(file_exists($controller_file_path)){
+        echo "ERROR file {$controller_file_path} is EXISTS, please DELETE";
+        return false;
+    }
+
+    $fp = fopen($controller_file_path, 'a+');
+    $fw = false;
+    if($mode == 'fpm'){
+        $fw = fwrite($fp, build_web_class($module, $controller));
+    }
+    if($mode == 'daemon'){
+        $fw = fwrite($fp, build_cli_class($module, $controller));
+    }
+    fclose($fp);
+
+    if($fw === false){
+        echo "ERROR write file {$controller_file_path} is FAILED".PHP_EOL;
+        return false;
+    }
+
+    echo "SUCCESS write file {$controller_file_path}".PHP_EOL;
+    return true;
 }
