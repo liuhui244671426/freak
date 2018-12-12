3d2e8ad6a7850580bbacecd2f446eae6487c258b
刘辉 <liuhui9@staff.weibo.com>
2018-12-12 15:57:14 +0800

    删除注释

--- freak/log.php
+++ freak/log.php
@@ -8,28 +8,6 @@
 defined('FREAK_ACCESS') or exit('Access Denied');
 class freak_log {
 
-    # @string, Log directory name
-    //private static $path = '';
-
-    # @void, Default Constructor, Sets the timezone and path of the log files.
-    public function __construct() {
-        //$this->path  = dirname(__FILE__)  . $this->path;
-        //self::$path = PATH_ROOT.DS.'logs'.DS;
-    }
-
-    /**
-     *   @void
-     *	Creates the log
-     *
-     *   @param string $message the message which is written into the log.
-     *	@description:
-     *	 1. Checks if directory exists, if not, create one and call this method again.
-     *	 2. Checks if log already exists.
-     *	 3. If not, new log gets created. Log is written into the logs folder.
-     *	 4. Logname is current date(Year - Month - Day).
-     *	 5. If log exists, edit method called.
-     *	 6. Edit method modifies the current log.
-     */
     public static function write($message, $pre='') {
         $date = new DateTime();
         $path = PATH_ROOT.DS.'logs'.DS;
@@ -53,15 +31,6 @@ class freak_log {
         }
     }
 
-    /**
-     *  @void
-     *  Gets called if log exists.
-     *  Modifies current log and adds the message to the log.
-     *
-     * @param string $log
-     * @param DateTimeObject $date
-     * @param string $message
-     */
     private static function edit($log,$date,$message) {
         $logcontent = "Time : " . $date->format('H:i:s')." Msg : " . $message ."\r\n\r\n";
         file_put_contents($log, $logcontent,FILE_APPEND);
