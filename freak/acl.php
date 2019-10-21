ddfb9fa7e3a98f0105d5f759683e31315f30efe3
liuhui9 <liuhui9@staff.weibo.com>
2019-10-21 15:52:49 +0800

    all

---
+++ freak/acl.php
@@ -0,0 +1,31 @@
+<?php
+//权限
+class freak_acl{
+    private $status = 0;
+    const ADD = 1;
+    const DEL = 2;
+    const SET = 4;
+    const GET = 8;
+
+
+    //设置
+    public function set($pid){
+        return $this->status = $this->status|$pid;
+    }
+    //添加
+    public function add($pid){
+        return $this->set($pid);
+    }
+    //删除
+    public function del($pid){
+        return $this->status = $this->status^$pid;
+    }
+    //权限
+    public function is($pid){
+        return $pid==($this->status&$pid);
+    }
+
+    public function getStatus(){
+        return $this->status;
+    }
+}
