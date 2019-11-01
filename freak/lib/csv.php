d42bc5a5b0b4c93b152c16c16711d8d5086f4cf2
liuhui9 <liuhui9@staff.weibo.com>
2019-11-01 18:49:55 +0800

    导出csv

---
+++ freak/lib/csv.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * 导出 csv 格式的 excel
+ * */
+class freak_lib_csv{
+
+    public function download($out_file_name , $header_data, $data, $limit=100000){
+        header('Content-Type: application/vnd.ms-excel');
+        header('Content-Disposition: attachment;filename='.$out_file_name);
+        header('Cache-Control: max-age=0');
+        $fp = fopen('php://output', 'a');
+        if (!empty($header_data)) {
+            foreach ($header_data as $key => $value) {
+                $header_data[$key] = iconv('utf-8', 'gbk', $value);
+            }
+            fputcsv($fp, $header_data);
+        }
+        $num = 0;
+        //每隔$limit行，刷新一下输出buffer，不要太大，也不要太小
+        //逐行取出数据，不浪费内存
+        $count = count($data);
+        if ($count > 0) {
+            for ($i = 0; $i < $count; $i++) {
+                $num++;
+                //刷新一下输出buffer，防止由于数据过多造成问题
+                if ($limit == $num) {
+                    ob_flush();
+                    flush();
+                    $num = 0;
+                }
+                $row = $data[$i];
+                foreach ($row as $key => $value) {
+                    $row[$key] = iconv('utf-8', 'gbk', $value);
+                }
+                fputcsv($fp, $row);
+            }
+        }
+        fclose($fp);
+        return true;
+    }
+}
